1067e5497bd4ef243bd726b5c7f42a683ca9bf13
Mark Ducommun <ducommun.mark@gmail.com>
2018-10-14 16:48:27 -0500

    Server can be stopped after each test now

--- src/Server.hs
+++ src/Server.hs
@@ -6,29 +6,52 @@ module Server
 import           Network
 import           Utilities
 import           System.IO
+import           Control.Concurrent.Chan
 
-startServer :: PortID -> IO ()
-startServer port = withSocketsDo $ do
+startServer :: (Chan Bool) -> PortID -> IO ()
+startServer channel port = withSocketsDo $ do
   socket <- listenOn port
-  loop socket
+  loop socket channel
 
-loop :: Socket -> IO()
-loop socket = do
+loop :: Socket -> (Chan Bool) -> IO ()
+loop socket channel = do
   (handle, _, _) <- accept socket
   headers <- readHeaders handle
+  respond handle headers
+  continueServing socket channel
+
+respond :: Handle -> [String] -> IO ()
+respond handle headers =
   case getPath headers of
-    (Just path) -> respondWithPath handle path
-    Nothing -> return ()
+    (Just path) -> writeResponse handle $ pathResponse path
+    Nothing -> writeResponse handle badRequest
+
+continueServing :: Socket -> (Chan Bool) -> IO ()
+continueServing socket channel = do
+  message <- readChan channel
+  if (message == True)
+  then do
+    writeChan channel True
+    loop socket channel
+  else sClose socket
+
+pathResponse :: String -> [String]
+pathResponse path =
+  [ "HTTP/1.1 200 OK\r\n"
+  , "Content-Length: " ++ (show $ length path) ++  "\r\n"
+  , "\r\n"
+  , path]
+
+badRequest :: [String]
+badRequest = ["HTTP/1.1 400 BAD REQUEST\r\n","Content-Length: 0\r\n","\r\n"]
+
+writeResponse :: Handle -> [String] -> IO ()
+writeResponse handle [] = do
   hFlush handle
   hClose handle
-  loop socket
-
-respondWithPath :: Handle -> String -> IO ()
-respondWithPath handle path = do
-  hPutStr handle "HTTP/1.1 200 OK\r\n"
-  hPutStr handle $ "Content-Length: " ++ (show $ length path) ++  "\r\n"
-  hPutStr handle "\r\n"
-  hPutStr handle path
+writeResponse handle (line:lines) = do
+  hPutStr handle line
+  writeResponse handle lines
 
 readHeaders :: Handle -> IO [String]
 readHeaders handle = do
--- test/ServerSpec.hs
+++ test/ServerSpec.hs
@@ -4,17 +4,35 @@ import           Control.Concurrent
 import           Test.Hspec
 import           Server
 import           Client
+import           ClientResponse
 import           Network
+import           System.IO
+
+port = PortNumber 8080
+
+withServer :: IO () -> IO ()
+withServer action = do
+  channel <- newChan
+  threadId <- forkIO $ startServer channel port
+  writeChan channel False
+  threadDelay 100
+  action
+  threadDelay 100
+  killThread threadId
 
 spec :: Spec
-spec = do
+spec = around_ withServer $ do
   describe "running a server" $ do
-    let port = PortNumber 8080
     let getClient = get "localhost" port
 
     it "responds to requests with the path" $ do
-      threadId <- forkIO $ startServer port
-      threadDelay 100
       response <- getClient "/hello"
       response `shouldBe` (OK $ Text "/hello")
-      killThread threadId
+
+    it "responds to requests without any path with bad request" $ do
+      withSocketsDo $ do
+        handle <- connectTo "localhost" port
+        hPutStr handle "GET\r\n\r\n\r\n"
+        hFlush handle
+        response <- handleResponse handle
+        response `shouldBe` (BAD_REQUEST Empty)
