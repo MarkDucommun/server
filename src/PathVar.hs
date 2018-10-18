748030255a6ed67f9a23384a07e33c2adabc18ed
Mark Ducommun <ducommun.mark@gmail.com>
2018-10-18 05:33:50 -0500

    Refactoring path var extraction

--- src/PathVar.hs
+++ src/PathVar.hs
@@ -20,9 +20,7 @@ findPathVars (_:_) (_:[]) = Nothing
 findPathVars (_:[]) (_:_) = Nothing
 findPathVars (a:as) (b:bs) =
   case extractPathVar a b of
-    (Var pathVar) -> do
-      pathVars <- findPathVars as bs
-      Just $ [pathVar] ++ pathVars
+    (Var pathVar) -> findPathVars as bs >>= \pathVars -> Just $ [pathVar] ++ pathVars
     Match -> findPathVars as bs
     NoMatch -> Nothing
 
@@ -31,10 +29,7 @@ extractPathVar ('{':remaining) aSegment = do
   case pathVarKey remaining of
     (Just key) -> Var (key, aSegment)
     Nothing -> NoMatch
-extractPathVar template aSegment =
-  if nonPathVarSegmentsEqual template aSegment
-  then Match
-  else NoMatch
+extractPathVar template aSegment = if template `equals` aSegment then Match else NoMatch
 
 pathVarKey :: String -> Maybe String
 pathVarKey ('}':[]) = Just []
@@ -43,10 +38,10 @@ pathVarKey (a:as) = do
   remaining <- pathVarKey as
   Just $ [a] ++ remaining
 
-nonPathVarSegmentsEqual :: String -> String -> Bool
-nonPathVarSegmentsEqual (a:[]) (b:[]) = a == b
-nonPathVarSegmentsEqual (_:_) (_:[]) = False
-nonPathVarSegmentsEqual (_:[]) (_:_) = False
-nonPathVarSegmentsEqual (a:as) (b:bs) = if a == b then nonPathVarSegmentsEqual as bs else False
+equals :: String -> String -> Bool
+equals (a:[]) (b:[]) = a == b
+equals (_:_) (_:[]) = False
+equals (_:[]) (_:_) = False
+equals (a:as) (b:bs) = if a == b then equals as bs else False
 
 data SegmentResult = Var PathVar | Match | NoMatch
