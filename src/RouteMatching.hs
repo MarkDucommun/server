15987405896674c9699f645c565f614383913cf3
Mark Ducommun <ducommun.mark@gmail.com>
2018-10-25 07:57:02 -0500

    improve get route matching

--- src/RouteMatching.hs
+++ src/RouteMatching.hs
@@ -17,27 +17,21 @@ import           Responses
 matchRoute :: [Route] -> Request -> IO Response
 matchRoute [] _ = return NOT_FOUND
 matchRoute (handler:handlers) (GetRequest path params) = matchGetRoute handler handlers path params
-matchRoute (handler:handlers) (PostRequest path body) = matchPostRoute' handler handlers path body
-matchRoute (handler:handlers) (PutRequest path body) = matchPutRoute' handler handlers path body
+matchRoute (handler:handlers) (PostRequest path body) = matchPostRoute handler handlers path body
+matchRoute (handler:handlers) (PutRequest path body) = matchPutRoute handler handlers path body
 
 matchGetRoute :: Route -> [Route] -> Path -> [Param] -> IO Response
-matchGetRoute (GET path (GetJustParams fn)) handlers thePath params =
-  if path == thePath
-    then getResponseToImpureResponse $ fn params
-    else matchRoute handlers $ GetRequest thePath params
-matchGetRoute (GET pathTemplate (GetJustPathVars fn)) handlers thePath params =
-  case pathVars pathTemplate thePath of
-    (Just thePathVars) -> getResponseToImpureResponse $ fn thePathVars
-    Nothing            -> matchRoute handlers $ GetRequest thePath params
-matchGetRoute (GET pathTemplate (GetParamsAndPathVars fn)) handlers thePath params =
-  case pathVars pathTemplate thePath of
-    (Just thePathVars) -> getResponseToImpureResponse $ fn (params, thePathVars)
+matchGetRoute (GET path handlerFn) handlers thePath params =
+  case pathVars path thePath of
+    (Just thePathVars) -> getResponseToImpureResponse $ invokeGetHandler handlerFn thePathVars params
     Nothing -> matchRoute handlers $ GetRequest thePath params
-matchGetRoute (GET path (GetStatic response)) remaining thePath params =
-  if path == thePath
-    then return response
-    else matchRoute remaining $ GetRequest thePath params
 matchGetRoute _ handlers path params = matchRoute handlers $ GetRequest path params
+
+invokeGetHandler :: GetRequestHandler -> [PathVar] -> [Param] -> GetResponse
+invokeGetHandler (GetJustParams fn) pathVars params = fn params
+invokeGetHandler (GetJustPathVars fn) pathVars params = fn pathVars
+invokeGetHandler (GetParamsAndPathVars fn) pathVars params = fn (params, pathVars)
+invokeGetHandler (GetStatic response) _ _ = Impure $ return $ response
 
 getResponseToImpureResponse :: GetResponse -> IO Response
 getResponseToImpureResponse response =
@@ -45,15 +39,15 @@ getResponseToImpureResponse response =
     (Impure response') -> response'
     (Pure response'')  -> return response''
 
-matchPostRoute' :: Route -> [Route] -> Path -> Maybe String -> IO Response
-matchPostRoute' (POST pathTemplate handlerFn) handlers thePath body =
+matchPostRoute :: Route -> [Route] -> Path -> Maybe String -> IO Response
+matchPostRoute (POST pathTemplate handlerFn) handlers thePath body =
   matchOrContinue PostRequest pathTemplate handlerFn handlers thePath body
-matchPostRoute' _ handlers thePath body = matchRoute handlers $ PostRequest thePath body
+matchPostRoute _ handlers thePath body = matchRoute handlers $ PostRequest thePath body
 
-matchPutRoute' :: Route -> [Route] -> Path -> Maybe String -> IO Response
-matchPutRoute' (PUT pathTemplate handlerFn) handlers thePath body =
+matchPutRoute :: Route -> [Route] -> Path -> Maybe String -> IO Response
+matchPutRoute (PUT pathTemplate handlerFn) handlers thePath body =
   matchOrContinue PutRequest pathTemplate handlerFn handlers thePath body
-matchPutRoute' _ handlers thePath body = matchRoute handlers $ PutRequest thePath body
+matchPutRoute _ handlers thePath body = matchRoute handlers $ PutRequest thePath body
 
 matchOrContinue :: (Path -> Maybe String -> Request) -> Path -> RequestWithBodyHandler -> [Route] -> Path -> Maybe String -> IO Response
 matchOrContinue requestType pathTemplate handlerFn handlers thePath body =
